523ef2688200151280351bd309cb8ed856bfc0cd
avinashvidyarthi <avinashvidyaryhi@gmail.com>
2020-07-16 14:53:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+  console.log("App running on port: " + port);
+});
+const io = require("socket.io").listen(server);
+
+
+app.use("/", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("./public/index.html");
+});
+
+io.on("connection", (socket) => {
+
+  socket.on("createOrJoin", (info) => {
+    const myRoom = io.sockets.adapter.rooms[info.roomName] || { length: 0 };
+    if (myRoom.length === 0) {
+      socket.join(info.roomName);
+      socket.emit("roomCreated", info);
+    } else if (myRoom.length === 1) {
+      socket.join(info.roomName);
+      socket.emit("roomJoined", info);
+    } else {
+      socket.emit("roomFull", info);
+    }
+  });
+
+  socket.on('ready',(info)=>{
+    socket.broadcast.to(info.roomName).emit('ready');
+  })
+
+  socket.on('candidate',(info)=>{
+    socket.broadcast.to(info.info.roomName).emit('candidate',info);
+  })
+
+  socket.on('offer',(info)=>{
+    socket.broadcast.to(info.info.roomName).emit('offer',info);
+  })
+
+  socket.on('answer',(info)=>{
+    socket.broadcast.to(info.info.roomName).emit('answer',info);
+  })
+  socket.on("disconnect", () => {
+  });
+});
+
+
---
+++ public/main.js
@@ -0,0 +1,137 @@
+const nameField = document.getElementById("nameField");
+const roomNameField = document.getElementById("roomNameField");
+const joinRoomBtn = document.getElementById("joinRoomBtn");
+const getDetailsDiv = document.getElementById("getDetailsDiv");
+const callingInterface = document.getElementById("callingInterface");
+const localVideo = document.getElementById("localVideo");
+const remoteVideo = document.getElementById("remoteVideo");
+
+let localStream, remoteStream, isCaller, info, rtcPeerConnection;
+
+const socket = io("http://localhost:3000");
+
+const localStreamConstrains = {
+  video: {
+    width: 240,
+    height: 240,
+  },
+  audio: true,
+  facingMode: "user",
+  frameRate: 15,
+};
+
+const iceServers = {
+  iceServer: [
+    { urls: "stun:stun.services.mozilla.com" },
+    { urls: "stun:stun.l.google.com:19302" },
+  ],
+};
+
+joinRoomBtn.onclick = () => {
+  if (nameField.value === "" || roomNameField.value === "") {
+    return swal("Error", "Name and Room Name must be filled", "error");
+  }
+
+  info = {
+    roomName: roomNameField.value,
+    userName: nameField.value,
+  };
+
+  socket.emit("createOrJoin", info);
+};
+
+socket.on("roomCreated", (info) => {
+  navigator.mediaDevices
+    .getUserMedia(localStreamConstrains)
+    .then((stream) => {
+      isCaller = true;
+      localStream = stream;
+      localVideo.srcObject = stream;
+      callingInterface.style.display = "block";
+      getDetailsDiv.style.display = "none";
+    })
+    .catch((err) => {
+      console.log(err);
+      swal("Error", "Something went wrong", "error");
+    });
+});
+
+socket.on("roomJoined", (info) => {
+  navigator.mediaDevices
+    .getUserMedia(localStreamConstrains)
+    .then((stream) => {
+      isCaller = false;
+      localStream = stream;
+      localVideo.srcObject = stream;
+      socket.emit("ready", info);
+      callingInterface.style.display = "block";
+      getDetailsDiv.style.display = "none";
+    })
+    .catch((err) => {
+      console.log(err);
+      swal("Error", "Something went wrong", "error");
+    });
+});
+
+socket.on("ready", (infor) => {
+  if (isCaller) {
+    rtcPeerConnection = new RTCPeerConnection(iceServers);
+    rtcPeerConnection.onicecandidate = onIceCandidate;
+    rtcPeerConnection.ontrack = addTrack;
+    rtcPeerConnection.addTrack(localStream.getTracks()[0], localStream);
+    rtcPeerConnection.addTrack(localStream.getTracks()[1], localStream);
+    rtcPeerConnection.createOffer().then((sessionDescription) => {
+      rtcPeerConnection.setLocalDescription(sessionDescription);
+      socket.emit("offer", {
+        sdp: sessionDescription,
+        info: info,
+      });
+    });
+  }
+});
+
+socket.on("offer", (infor) => {
+  if (!isCaller) {
+    rtcPeerConnection = new RTCPeerConnection(iceServers);
+    rtcPeerConnection.onicecandidate = onIceCandidate;
+    rtcPeerConnection.ontrack = addTrack;
+    rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(infor.sdp));
+    rtcPeerConnection.addTrack(localStream.getTracks()[0], localStream);
+    rtcPeerConnection.addTrack(localStream.getTracks()[1], localStream);
+    rtcPeerConnection.createAnswer().then((sessionDescription) => {
+      rtcPeerConnection.setLocalDescription(sessionDescription);
+      socket.emit("answer", {
+        sdp: sessionDescription,
+        info: info,
+      });
+    });
+  }
+});
+
+socket.on("answer", (info) => {
+  rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(info.sdp));
+});
+
+socket.on('candidate',(event)=>{
+  const candidate=new RTCIceCandidate({
+    sdpMLineIndex:event.label,
+    candidate:event.candidate
+  })
+  rtcPeerConnection.addIceCandidate(candidate);
+})
+
+function addTrack(event) {
+  remoteVideo.srcObject = event.streams[0];
+  remoteStream = event.streams[0];
+}
+
+function onIceCandidate(event){
+  if(event.candidate){
+    socket.emit('candidate',{
+      label:event.candidate.sdpMLineIndex,
+      id:event.candidate.sdpMid,
+      candidate:event.candidate.candidate,
+      info:info
+    })
+  }
+}
